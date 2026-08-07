59baed14893a43f7e0490f7bfa75336de4a647be
dfarnworth1-sketch <dfarnworth1@googlemail.com>
2026-08-07 21:58:06 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -372,6 +372,25 @@ const toolkitData = [
       title: "GDE Matrix PDF",
       type: "pdf",
       url: "GDE-matrix.pdf"
+     name: "Driving Test",
+    icon: "📋",
+    topics: [
+      {
+        name: "Show Me, Tell Me",
+        videos: [
+      {
+        title: "Show Me, Tell Me" ,
+        id: "uh8slnP76-w"
+          }
+        ]
+      },
+     { 
+        name: "Independent Driving",
+        videos: [
+          {
+            title: "Using a Sat Nav on the Driving Test",
+            id: "SaapXrhuEp8"
+         
     }
   ]
 },
